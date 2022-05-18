5eb6258454c7f782b93364f0e0ba335b2f3a987c
aonopko <aonopko.rpu@gmail.com>
2022-05-18 23:49:05 +0300

    add list_url

--- pars_ager.py
+++ pars_ager.py
@@ -3,11 +3,12 @@ import requests
 from bs4 import BeautifulSoup
 
 HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
-}
+     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36",
+     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
+ }
 
 URL = "https://ager.ua/women/zhenskie-dzhinsy/"
+
 
 
 def get_html():
@@ -24,11 +25,17 @@ def open_file():
 
 def get_cart():
     soup = BeautifulSoup(open_file(), "lxml")
-    product_href = soup.find("div", class_="image")
+    product_href = soup.find_all("div", class_="image")
     return product_href
 
 
 def get_href():
-    href = get_cart().find("a").get("href")
-    return href
+    list_url = []
+    for i in get_cart():
+        href = i.find("a").get("href")
+        list_url.append(href)
+    return list_url
+
+
+
 
