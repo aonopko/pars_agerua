504eafda6cf07867fe7bad5967368578ce478fbb
aonopko <aonopko.rpu@gmail.com>
2022-05-28 20:43:54 +0300

    add test

--- pars_ager.py
+++ pars_ager.py
@@ -1,13 +1,17 @@
 import requests
 
+from dotenv import load_dotenv
+
 from bs4 import BeautifulSoup
 
-HEADERS = {
-     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36",
-     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
- }
 
-URL = "https://ager.ua/women/zhenskie-dzhinsy/"
+load_dotenv('.env')
+
+
+
+HEADERS = "HEADERS"
+
+URL = "URL"
 
 
 
